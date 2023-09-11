9b20d3a504fb65a91d68090a445b364fd4891e37
frupreneur <frupreneur@gmail.com>
2023-09-11 15:57:20 +0100

    updated links

--- app/page.js
+++ app/page.js
@@ -42,16 +42,16 @@ fbq('track', 'PageView');`,
             Hello, what do you need help with? <br />
             Click answer below👇👇
           </h3>
-          <Link href="https://travel.thewordmixer.com/2023/05/high-paying-jobs-in-canada-for.html">
+          <Link href="https://travel.englishcompositions.com/2023/05/high-paying-jobs-in-canada-for.html">
             🇨🇦 GET STARTED 🇨🇦
           </Link>
-          <Link href="https://travel.thewordmixer.com/2023/05/high-paying-jobs-in-canada-for.html">
+          <Link href="https://travel.englishcompositions.com/2023/05/high-paying-jobs-in-canada-for.html">
             🇨🇦 CANADA VISA 🇨🇦{" "}
           </Link>
-          <Link href="https://travel.thewordmixer.com/2023/05/high-paying-jobs-in-canada-for.html">
+          <Link href="https://travel.englishcompositions.com/2023/05/high-paying-jobs-in-canada-for.html">
             🇨🇦 STUDY IN CANADA 🇨🇦
           </Link>
-          <Link href="https://travel.thewordmixer.com/2023/05/high-paying-jobs-in-canada-for.html">
+          <Link href="https://travel.englishcompositions.com/2023/05/high-paying-jobs-in-canada-for.html">
             🇨🇦 JOBS IN CANADA 🇨🇦
           </Link>
         </div>
